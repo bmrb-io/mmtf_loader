b7abf69d0c881f44828f43169053e4a96a4556e2
Jon Wedell <wedell@bmrb.wisc.edu>
2017-08-15 11:46:03 -0500

    Added method to parse mmtfs from redis.

--- search.py
+++ search.py
@@ -2,9 +2,12 @@
 
 import re
 import sys
+import zlib
 import json
 import redis
-from mmtf import parse_gzip
+import msgpack
+from StringIO import StringIO
+from mmtf import parse_gzip, MMTFDecoder
 
 sequences = json.load(open("data/ss.json",'r'))
 
@@ -12,6 +15,9 @@ def contains(str1, distance, str2):
     """ Check if search strings exist in DB separated by distance. """
 
     matches = []
+
+    str1 = str1.upper()
+    str2 = str2.upper()
 
     for seq in sequences:
         if str1 in seq and str2 in seq:
@@ -24,21 +30,32 @@ def contains(str1, distance, str2):
 
     return sorted(list(set(matches)))
 
-def get_mmtfs(str1, distance, str2):
+def get_mmtfs(str1, distance, str2, parsed=True):
     """ Returns a list of mmtf objects for PDB IDs that have str1 separated
     from str2 by distance."""
 
     pdbs = contains(str1, distance, str2)
-    results = []
 
     red = redis.Redis()
     for pdb in pdbs:
         print("Fetching %s from red." % pdb)
-        gzipped_mmtf = red.get(pdb)
-        if not gzipped_mmtf:
+        mmtf = red.get(pdb)
+
+        if not mmtf:
             print("Missing %s" % pdb)
             #raise ValueError("Could not find PDB %s in Redis!" % pdb)
             continue
-        results.append(parse_gzip(gzipped_mmtf))
 
-    return results
+    if parsed:
+        yield parse_mmtf(mmtf)
+    else:
+        yield mmtf
+
+def parse_mmtf(mmtf):
+    """ Loads the MMTF objects. """
+
+    mmtf_decoder = MMTFDecoder()
+    mmtf = zlib.decompress(mmtf, 16+zlib.MAX_WBITS)
+    mmtf = msgpack.unpackb(mmtf)
+    mmtf_decoder.decode_data(mmtf)
+    yield mmtf_decoder
