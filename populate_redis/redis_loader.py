1799bbd550664105ff1760c5d332ce8241ed4b9d
Jon Wedell <wedell@bmrb.wisc.edu>
2017-08-16 14:16:55 -0500

    Multicore the redis loading code.

--- populate_redis/redis_loader.py
+++ populate_redis/redis_loader.py
@@ -6,6 +6,8 @@ import zlib
 import redis
 import msgpack
 from collections import deque
+import multiprocessing
+
 
 from mmtf import parse_gzip, MMTFDecoder
 
@@ -36,19 +38,36 @@ def extract_important(mmtf):
         if max_res == 0:
             return zlib.compress(msgpack.dumps(residue_list))
 
+def chunker(l, desired_sublists):
+    """ Breaks a list in x sublists of roughly equal size. """
 
-file_list = ["full/" + x for x in filter(lambda x:"part-" in x, os.listdir("full"))]
+    res = [[] for x in range(0,desired_sublists)]
+    c = desired_sublists
+
+    while len(l) > 0:
+        res[c-1].append(l.pop())
+        c = (c - 1) % desired_sublists
+    return res
+
 redis_conn = redis.Redis()
+file_list = ["full/" + x for x in filter(lambda x:"part-" in x, os.listdir("full"))]
+cores = multiprocessing.cpu_count()
+chunked_list = chunker(file_list, cores)
 
-for ef in file_list:
-    print(ef)
-    reader = SequenceFile.Reader(ef)
-    kc = reader.getKeyClass()
-    vc = reader.getValueClass()
+for x in range(0, cores):
+    pid = os.fork()
+    if pid == 0:
 
-    k,v = kc(), vc()
+        for ef in chunked_list[x]:
+            print("Proc %s doing %s" % (x, ef))
+            reader = SequenceFile.Reader(ef)
+            kc = reader.getKeyClass()
+            vc = reader.getValueClass()
 
-    while reader.next(k,v):
-        ks = k.toString()
-        redis_conn.set(ks, extract_important(v.toString()))
-        print("%s" % ks)
+            k,v = kc(), vc()
+
+            while reader.next(k,v):
+                ks = k.toString()
+                redis_conn.set(ks, extract_important(v.toString()))
+                print("%s" % ks)
+        sys.exit(0)
