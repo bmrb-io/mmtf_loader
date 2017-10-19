d2f5c2e81c6d34e81ed40af64c39cbbac80a707c
Jon Wedell <wedell@bmrb.wisc.edu>
2017-10-19 14:55:18 -0500

    Checkpointing before refactor.

--- search.py
+++ search.py
@@ -4,6 +4,8 @@ import sys
 import zlib
 from re import finditer
 
+from collections import deque
+
 import redis
 import msgpack
 
@@ -18,6 +20,7 @@ def _contains(str1, str2, distance_min=None, distance_max=None):
     str1 = str1.upper()
     str2 = str2.upper()
     str1l = len(str1)
+    str2l = len(str2)
 
     # Set the defaults
     if distance_min is None:
@@ -25,23 +28,27 @@ def _contains(str1, str2, distance_min=None, distance_max=None):
 
     if distance_max is None:
         distance_max = distance_min
+    distance_max += 1
 
     for seq in sequences:
         if str1 in seq and str2 in seq:
-            s1idx = [m.start() for m in finditer('(?=%s)' % str1, seq)]
-            if str1 == str2:
-                s2idx = s1idx
-            else:
-                s2idx = [m.start() for m in finditer('(?=%s)' % str2, seq)]
+            s1idx = [m.start()+str1l for m in finditer('(?=%s)' % str1, seq)]
+            s2idx = deque([m.start()+s1idx[0] for m in finditer('(?=%s)' % str2, seq[s1idx[0]:s1idx[-1] + str2l])])
+            if len(s2idx) == 0:
+                continue
 
             for idx in s1idx:
-                for diter in range(distance_min + str1l + idx, distance_max + 1 + str1l + idx):
+                while len(s2idx) > 0 and idx > s2idx[0]:
+                    s2idx.popleft()
+                if len(s2idx) == 0:
+                    continue
+                for diter in range(distance_min + idx + str1l, distance_max + idx + str1l):
                     if diter in s2idx:
                         for pdb in set(sequences[seq]):
                             try:
-                                matches[pdb].add((idx, diter - str1l - idx))
+                                matches[pdb].add((idx - str1l, diter - str1l - idx))
                             except KeyError:
-                                matches[pdb] = set([(idx, diter- str1l - idx)])
+                                matches[pdb] = set([(idx - str1l, diter - str1l - idx)])
 
     clean_res = []
 
@@ -59,13 +66,16 @@ def _fake_redis(pdbs):
         except IOError:
             yield None
 
-def get_coords(str1, str2, distance_min, distance_max=None):
+def get_coords(str1, str2, distance_min=None, distance_max=None, debug=False):
     """ Returns a list of mmtf objects for PDB IDs that have str1 separated
     from str2 by distance_min, or if distance_max is provided, that have str1
     separated from str2 by any distance in the range
     [distance_min - distance_max]."""
 
     pdbs = _contains(str1, str2, distance_min, distance_max)
+
+    if debug:
+        yield pdbs
 
     pure_ids = [x[0] for x in pdbs]
 
